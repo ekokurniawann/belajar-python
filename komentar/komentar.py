7430d344f121a9ba473142d2d0faf634fe73209f
ekokurniawann <ekokurniawaann@gmail.com>
2024-03-15 21:54:36 +0700

    komentar-python

---
+++ komentar/komentar.py
@@ -0,0 +1,10 @@
+# komentar satu baris
+# ini adalah komentar
+print("halo, python")
+print("belajar python itu menyenangkan") # ini juga komentar
+
+# print("ini tidak akan di eksekusi")
+
+"""
+ini komentar multi baris
+"""
