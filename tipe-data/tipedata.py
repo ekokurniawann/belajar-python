711527b3438e1083cf21979261c966c02a5e9b4c
ekokurniawann <ekokurniawaann@gmail.com>
2024-03-17 12:58:44 +0700

    tipe data python

---
+++ tipe-data/tipedata.py
@@ -0,0 +1,58 @@
+# Tipe data numerik
+number_1 = 10000024
+number_2 = 3.14
+number_3 = 120+3j  # Tipe data kompleks
+
+# Tipe data string
+string_1 = "hello python"
+string_2 = """Selamat
+Belajar
+Python"""
+string_3 = 'for the horde!'
+string_4 = '''
+Sesuk
+Preiiii
+'''
+string_5 = '''\
+Sesuk
+Preiiii
+'''
+
+# Tipe data bool
+bool_1 = True
+bool_2 = False
+
+# Tipe data list
+list_1 = [2, 4, 8, 16]
+list_2 = ["grayson", "jason", "tim", "damian"]
+list_3 = [24, False, "Hello Python"]
+
+# Mengakses element list
+print("Elemen ke-3 dari list_1:", list_1[2])  # Output: 8
+
+# Tipe data tuple
+tuple_1 = (2, 3, 4)
+tuple_2 = ("numenor", "valinor")
+tuple_3 = (24, False, "Hello Python")
+
+# Mengakses element tuple
+print("Elemen ke-3 dari tuple_1:", tuple_1[2])  # Output: 4
+
+# Tipe data dictionary
+profile_1 = {
+  "name": "Roger",
+  "is_male": False,
+  "age": 16,
+  "hobbies": ["gaming", "learning"]
+}
+
+# Mengakses property dictionary
+print("Nama:", profile_1["name"])  # Output: Nama: Roger
+print("Hobi:", profile_1["hobbies"])  # Output: Hobi: ['gaming', 'learning']
+
+# Tipe data set
+set_1 = {"pineapple", "spaghetti"}
+print("Elemen dalam set_1:", set_1)  # Output: Elemen dalam set_1: {'pineapple', 'spaghetti'}
+
+# Tipe data kompleks
+print("Nilai kompleks:", number_3)  # Output: Nilai kompleks: (120+3j)
